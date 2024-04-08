972cb7aa8dfc6c9c372f7ebbcbad2ac6ccdd346b
Anton Giertli <agiertli@redhat.com>
2024-04-08 12:18:26 +0200

    fix wrapper

--- argo-apps/workshop-setup/wrapper.sh
+++ argo-apps/workshop-setup/wrapper.sh
@@ -1,1 +1,2 @@
-./execute-in-pods.sh "controller.devfile.io/devworkspace_name=superheroes-workshop-seeded" "mvn clean install -Pcomplete -DskipTests -f /projects/superheroes-workshop/quarkus-workshop-super-heroes/pom.xml"
+./execute-in-pods.sh "controller.devfile.io/devworkspace_name=superheroes-workshop-seeded" "mvn clean install -Pcomplete -DskipTests -f /projects/superheroes-workshop-seeded/quarkus-workshop-super-heroes/pom.xml"
+

51dcdc39e9e677e4e296eeab57bcc70698809069
Anton Giertli <agiertli@redhat.com>
2024-04-09 16:48:31 +0200

    some more images

--- argo-apps/workshop-setup/wrapper.sh
+++ argo-apps/workshop-setup/wrapper.sh
@@ -1,2 +1,2 @@
 ./execute-in-pods.sh "controller.devfile.io/devworkspace_name=superheroes-workshop-seeded" "mvn clean install -Pcomplete -DskipTests -f /projects/superheroes-workshop-seeded/quarkus-workshop-super-heroes/pom.xml"
-
+#./execute-in-pods.sh "controller.devfile.io/devworkspace_name=superheroes-workshop-seeded" "cd /projects/superheroes-workshop-seeded && git stash && git pull --rebase"
